37b588556edf488201c6813a9e5164cf39495eb5
Jonatan Alvarez <alvarez.geek@hotmail.com>
2021-12-11 14:43:06 -0500

    Se configura enviroments

--- src/app/services/postulante.service.ts
+++ src/app/services/postulante.service.ts
@@ -2,27 +2,26 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Postulante } from '../Model/Postulante';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostulanteService {
 
-  private baseUrl = 'http://localhost:8050/formularios';
-
   private headers: HttpHeaders = new HttpHeaders({ 'Content-Type': 'application/json' });
 
   constructor(private httpClient: HttpClient) { }
 
   consultarPostulante(): Observable<Postulante[]> {
-    return this.httpClient.get<Postulante[]>(`${this.baseUrl}/consultarFormularios`);
+    return this.httpClient.get<Postulante[]>(`${environment.apiUrl}/consultarFormularios`);
   }
 
   actualizarPostulante(postulante: Postulante): Observable<Postulante> {
-    return this.httpClient.put<Postulante>(`${this.baseUrl}/actualizarFormulario`, postulante, { headers: this.headers });
+    return this.httpClient.put<Postulante>(`${environment.apiUrl}/actualizarFormulario`, postulante, { headers: this.headers });
   }
 
   eliminarPostulante(id: number): Observable<void> {
-    return this.httpClient.delete<void>(`${this.baseUrl}/eliminarFormulario/${id}`);
+    return this.httpClient.delete<void>(`${environment.apiUrl}/eliminarFormulario/${id}`);
   }
 }
